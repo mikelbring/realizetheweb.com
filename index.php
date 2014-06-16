d8158997251180118e131ba389e8de2f0ab5ed5a
Suthan S <suthans90@gmail.com>
2014-06-16 14:19:19 -0400

    fix

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-index.
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Realize the Web</title>
+		<link rel="stylesheet" href="stylesheets/screen.css" />
+	</head>
+	<body>
+
+		<section class="section welcome">
+			welcome
+		</div>
+
+	</body>
+</html>

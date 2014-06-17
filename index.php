75fcf7ec186e62ef3edb96ebe455fb142b2d979c
Suthan S <suthans90@gmail.com>
2014-06-17 17:07:33 -0400

    website design update

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html>
 	<head>
 		<meta charset="UTF-8" />
 		<title>Realize the Web</title>
-		<link rel="stylesheet" href="stylesheets/screen.css" />
+
+		<link rel="stylesheet" type="text/css" href="stylesheets/screen.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheets/fullpage.css" />
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="vendors/easings.min.js"></script>
+		<script type="text/javascript" src="vendors/slimscroll.min.js"></script>
+		<script type="text/javascript" src="js/fullpage.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
 	</head>
 	<body>
+		<div id="main">
+			<section class="section welcome">	
+				<div class="row">
+					<h1><span>realize</span>theweb</h1>
+					<p>We are a small creative team of product designers and developers. Send us an <a href="mailto:hello@realizetheweb.com">email</a> if you would like to chat about what we can do for you. Also check out some of the in-house products we have built below.</p>
+					<a href="#">We're available for work</a>
+				</div>
+				<i class="arrow"></i>
+			</section>
 
-		<section class="section welcome">
-			welcome
+			<section class="section fllix">
+				<div class="row">
+					
+					<a href="http://www.fllix.com" class="logo left">Fllix</a>
+					<a href="#" class="right">Visit Website</a>
+					<p>
+						Fllix is a social network built for movie addicts. Browse, build and share your collection of movies with your family and friends at fllix.com!
+					</p>
+					<img src="images/projects/fllix.png" />
+
+				</div>
+			</section>
 		</div>
-
 	</body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#main').fullpage({
+        verticalCentered: true,
+        resize : false,
+    });
+});

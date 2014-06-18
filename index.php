a9111efed2b1bf48f5db183535e0d18b6db06486
Suthan S <suthans90@gmail.com>
2014-06-18 10:40:52 -0400

    fixed broken links

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 				<div class="row">
 					<h1><span>realize</span>theweb</h1>
 					<p>We are a small creative team of product designers and developers. Send us an <a href="mailto:hello@realizetheweb.com">email</a> if you would like to chat about what we can do for you. Also check out some of the in-house products we have built below.</p>
-					<a href="#">We're available for work</a>
+					<a href="mailto:hello@realizetheweb.com">We're available for work</a>
 				</div>
 				<i class="arrow"></i>
 			</section>
@@ -28,7 +28,7 @@
 				<div class="row">
 					
 					<a href="http://www.fllix.com" class="logo left">Fllix</a>
-					<a href="#" class="right">Visit Website</a>
+					<a href="http://www.fllix.com" class="right">Visit Website</a>
 					<p>
 						Fllix is a social network built for movie addicts. Browse, build and share your collection of movies with your family and friends at fllix.com!
 					</p>
